9535cf49d640e95f95651f60f0a354b8eee2a4f5
warfora <wildosaff@gmail.com>
2022-12-02 16:51:19 +0200

    button in projects

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Heroimg4 from "../components/Heroimg4";
 import ImageSlider from "../components/ImageSlider";
+import {Link} from 'react-router-dom'
 
 import { SliderData } from '../components/SliderData'
 
@@ -14,7 +15,13 @@ const Project = () => {
       <Navbar/>
       <Heroimg4 heading="PROJECTS." text="My most recent works"/>
       <ImageSlider slides={SliderData}/>
-      <img className="campfire2" src="https://iili.io/Hqq8n8F.png" alt="campfire2"/>
+      <div className="linking">
+      <Link to="/about">
+                <button className="btn">
+                    About me
+                </button>
+            </Link>
+      </div>
       <Footer/>
     </div>
   )

e1129d4b27d859575a67e9499c659da963cfb1cc
warfora <wildosaff@gmail.com>
2022-12-02 14:50:17 +0200

    media

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import "./Project.css"
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Heroimg4 from "../components/Heroimg4";
-//import WorkCard from "../components/WorkCard";
+import ImageSlider from "../components/ImageSlider";
+
+import { SliderData } from '../components/SliderData'
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
       <Heroimg4 heading="PROJECTS." text="My most recent works"/>
-      
+      <ImageSlider slides={SliderData}/>
       <Footer/>
     </div>
   )

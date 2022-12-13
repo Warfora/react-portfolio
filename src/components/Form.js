56bc73241f62d1f83c0c49e632e6ca722a0a9766
warfora <wildosaff@gmail.com>
2022-12-13 18:57:40 +0200

    sumbit done

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,21 +2,47 @@ import "./FormStyles.css"
 
 import React from 'react'
 
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 const Form = () => {
+
+  const form = useRef();
+  const inputs = useRef();
+  const inputs1 = useRef();
+  const inputs2 = useRef();
+  const inputs3 = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rj3l5jo', 'template_f0591am', form.current, '6NxC1ve2bEoMuROli')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      inputs.current.value = "";
+      inputs1.current.value = "";
+      inputs2.current.value = "";
+      inputs3.current.value = "";
+  };
+
   return (
     <div className="form">
-        <form>
-            <label>Your Name</label>
-            <input type="text"></input>
-            <label>Email</label>
-            <input type="email"></input>
-            <label>Subject</label>
-            <input type="text"></input>
-            <label>Message</label>
-            <textarea rows ="6" placeholder="Type your message here"/>
-            <button className="btn">Submit</button>
-            <br/><br/>
-        </form>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Your Name</label>
+          <input ref={inputs} type="text" name="user_name" />
+          <label>Email</label>
+          <input ref={inputs1} type="email" name="user_email" />
+          <label>Subject</label>
+          <input ref={inputs2} type="text" name="user_subject"/>
+          <label>Message</label>
+          <textarea ref={inputs3} rows ="6" placeholder="Type your message here" name="message" />
+          <input className="btn" type="submit" value="Submit" />
+          <br/><br/>
+       </form>
     </div>
   )
 }
